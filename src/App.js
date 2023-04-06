2d39426b316e82cdc61f7e8c880959081e79088f
Bruno H Cardoso <riquebruno@yahoo.com.br>
2023-04-06 19:16:19 -0300

    Requisito 5 Ok

--- src/App.js
+++ src/App.js
@@ -16,23 +16,35 @@ class App extends React.Component {
     isSaveButtonDisabled: 'true',
   };
 
-  handleChange = ({ target: { value, name } }) => {
+  buttonVerify = () => {
+    const { cardName, cardDescription, cardAttr1,
+      cardAttr2, cardAttr3, cardImage, cardRare } = this.state;
+    const maxValue = 90;
+    const maxTotalValue = 210;
+    const atr1 = Number(cardAttr1) >= 0 && Number(cardAttr1) <= maxValue;
+    const atr2 = Number(cardAttr2) >= 0 && Number(cardAttr2) <= maxValue;
+    const atr3 = Number(cardAttr3) >= 0 && Number(cardAttr3) <= maxValue;
+    const name = cardName.length > 0;
+    const descript = cardDescription.length > 0;
+    const rarity = cardRare.length > 0;
+    const Image = cardImage.length > 0;
+    const atr = Number(cardAttr1) + Number(cardAttr2)
+      + Number(cardAttr3) <= maxTotalValue;
     this.setState({
-      [name]: value,
+      isSaveButtonDisabled: !(name && rarity
+         && Image && descript && atr1 && atr2 && atr3 && atr),
     });
   };
 
+  handleChange = ({ target: { value, name } }) => {
+    this.setState({
+      [name]: value,
+    }, this.buttonVerify);
+  };
+
   render() {
-    const {
-      cardName,
-      cardDescription,
-      cardAttr1,
-      cardAttr2,
-      cardAttr3,
-      cardImage,
-      cardRare,
-      cardTrunfo,
-      // hasTrunfo,
+    const { cardName, cardDescription, cardAttr1, cardAttr2,
+      cardAttr3, cardImage, cardRare, cardTrunfo, // hasTrunfo,
       isSaveButtonDisabled,
     } = this.state;
     return (
