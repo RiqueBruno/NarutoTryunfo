2fae593da4402679456a2e20dad788b98c99afdf
Bruno <riquebruno@yahoo.com.br>
2024-08-19 12:18:30 -0300

    Style: CSS Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,31 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import logo from '../../assets/Logo.png'
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/Logo.png";
 
-import './Header.css'
+import "./Header.css";
 
 function Header() {
   return (
-    <header className='header'>
-        <Link className='imgLink' to="/">
-          <img src={logo} alt="Icon Naruto Triunfo" />
+    <header className="header">
+      <Link className="imgLink" to="/">
+        <img src={logo} alt="Icon Naruto Triunfo" />
+      </Link>
+      <nav className="navHeader">
+        <Link className="linkClass" to="/">
+          Home
         </Link>
-        <nav>
-          <Link className='linkClass' to="/">Home</Link>
-          <Link className='linkClass' to="/collection">Collection</Link>
-          <Link className='linkClass' to="/game">Play</Link>
-          <Link className='linkClass' to="/rank">Rank</Link>
-        </nav>
+        <Link className="linkClass" to="/collection">
+          Collection
+        </Link>
+        <Link className="linkClass" to="/game">
+          Play
+        </Link>
+        <Link className="linkClass" to="/rank">
+          Rank
+        </Link>
+      </nav>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
