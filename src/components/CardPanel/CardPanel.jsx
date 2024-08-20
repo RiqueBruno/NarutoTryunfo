a11ac1ede26ceec183e7307a750dcc5675c51c1f
Bruno <riquebruno@yahoo.com.br>
2024-08-20 19:03:21 -0300

    Fix: Erro de tela branca quando acabavam as cartas

--- src/components/CardPanel/CardPanel.jsx
+++ src/components/CardPanel/CardPanel.jsx
@@ -40,6 +40,16 @@ function CardPanel({ collection }) {
   };
 
   const onClickAtribute = ({ target: { id } }) => {
+    if (cardsPlayer.length <= 0) {
+      setPanel((prevPanel) => ({
+        ...prevPanel,
+        start: false,
+        turn: false,
+        final: true,
+      }));
+      return;
+    }
+
     //inicio do turno
     setPanel((prevPanel) => ({ ...prevPanel, turn: !prevPanel.turn }));
     const plrCard = cardStatus.plrCard;
@@ -84,66 +94,71 @@ function CardPanel({ collection }) {
   };
 
   return (
-    <div className="cardPanel">
-      <div className="resultGame">
-        {panel.final ? <Result cardStatus={cardStatus} /> : ""}
-      </div>
-      <div className="playerSide">
-        <div className="scorePlayer">{`Win: ${cardStatus.plrScore}`}</div>
-        <div className="deckPlayer">
-          <div className="deckPlayer1"></div>
-          <div className="deckPlayer2"></div>
-          <div className="deckPlayer3"></div>
-          <div className="deckPlayer4"></div>
-          <div className="deckPlayer5"></div>
+    <div className="panel">
+      {panel.final ? (
+        <div className="resultGame">
+          <Result cardStatus={cardStatus} />
         </div>
-        <div className={`cardFlip cardFlip${panel.start}`}>
-          <div className={`card-container`}>
-            <div className="card-container1">
-              <Card
-                cardName={cardStatus.plrCard.cardName}
-                cardDescription={cardStatus.plrCard.cardDescription}
-                cardAttr1={cardStatus.plrCard.cardAttr1.toString()}
-                cardAttr2={cardStatus.plrCard.cardAttr2.toString()}
-                cardAttr3={cardStatus.plrCard.cardAttr3.toString()}
-                cardImage={cardStatus.plrCard.cardImage}
-                cardRare={cardStatus.plrCard.cardRare}
-                cardTrunfo={cardStatus.plrCard.cardTrunfo}
-              />
-              <div className="btnPanel">
-                <button id="cardAttr1" onClick={onClickAtribute} />
-                <button id="cardAttr2" onClick={onClickAtribute} />
-                <button id="cardAttr3" onClick={onClickAtribute} />
+      ) : (
+        <div className="cardPanel">
+          <div className="playerSide">
+            <div className="scorePlayer">{`Win: ${cardStatus.plrScore}`}</div>
+            <div className="deckPlayer">
+              <div className="deckPlayer1"></div>
+              <div className="deckPlayer2"></div>
+              <div className="deckPlayer3"></div>
+              <div className="deckPlayer4"></div>
+              <div className="deckPlayer5"></div>
+            </div>
+            <div className={`cardFlip cardFlip${panel.start}`}>
+              <div className={`card-container`}>
+                <div className="card-container1">
+                  <Card
+                    cardName={cardStatus.plrCard.cardName}
+                    cardDescription={cardStatus.plrCard.cardDescription}
+                    cardAttr1={cardStatus.plrCard.cardAttr1.toString()}
+                    cardAttr2={cardStatus.plrCard.cardAttr2.toString()}
+                    cardAttr3={cardStatus.plrCard.cardAttr3.toString()}
+                    cardImage={cardStatus.plrCard.cardImage}
+                    cardRare={cardStatus.plrCard.cardRare}
+                    cardTrunfo={cardStatus.plrCard.cardTrunfo}
+                  />
+                  <div className="btnPanel">
+                    <button id="cardAttr1" onClick={onClickAtribute} />
+                    <button id="cardAttr2" onClick={onClickAtribute} />
+                    <button id="cardAttr3" onClick={onClickAtribute} />
+                  </div>
+                </div>
+              </div>
+              <div className={`backCardPlayerDiv`}>
+                <div className="backCardPlayer">
+                  <button onClick={getCard}>Get Card</button>
+                </div>
               </div>
             </div>
           </div>
-          <div className={`backCardPlayerDiv`}>
-            <div className="backCardPlayer">
-              <button onClick={getCard}>Get Card</button>
+          <div className="computerSide">
+            <div>{cardStatus.compScore}</div>
+            <div>deck de costas</div>
+            <div>
+              {panel.turn ? (
+                <Card
+                  cardName={cardStatus.compCard.cardName}
+                  cardDescription={cardStatus.compCard.cardDescription}
+                  cardAttr1={cardStatus.compCard.cardAttr1.toString()}
+                  cardAttr2={cardStatus.compCard.cardAttr2.toString()}
+                  cardAttr3={cardStatus.compCard.cardAttr3.toString()}
+                  cardImage={cardStatus.compCard.cardImage}
+                  cardRare={cardStatus.compCard.cardRare}
+                  cardTrunfo={cardStatus.compCard.cardTrunfo}
+                />
+              ) : (
+                <div>Carta de costas</div>
+              )}
             </div>
           </div>
         </div>
-      </div>
-      <div className="computerSide">
-        <div>{cardStatus.compScore}</div>
-        <div>deck de costas</div>
-        <div>
-          {panel.turn ? (
-            <Card
-              cardName={cardStatus.compCard.cardName}
-              cardDescription={cardStatus.compCard.cardDescription}
-              cardAttr1={cardStatus.compCard.cardAttr1.toString()}
-              cardAttr2={cardStatus.compCard.cardAttr2.toString()}
-              cardAttr3={cardStatus.compCard.cardAttr3.toString()}
-              cardImage={cardStatus.compCard.cardImage}
-              cardRare={cardStatus.compCard.cardRare}
-              cardTrunfo={cardStatus.compCard.cardTrunfo}
-            />
-          ) : (
-            <div>Carta de costas</div>
-          )}
-        </div>
-      </div>
+      )}
       <div className="scoreTotalGame">{cardStatus.totalScore}</div>
     </div>
   );
