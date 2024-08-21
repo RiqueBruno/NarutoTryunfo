f94de0c9d61d6c73902f0e7c7c622680e743c021
Bruno <riquebruno@yahoo.com.br>
2024-08-21 14:02:38 -0300

    Style: CSS rank page e RankPanel componente

--- src/components/RankPanel/RankPanel.jsx
+++ src/components/RankPanel/RankPanel.jsx
@@ -1,30 +1,33 @@
 import PropTypes from "prop-types";
 import React from "react";
+import "./RankPanel.css";
 
 function RankPanel({ theRank }) {
   const rank = [...theRank].sort((a, b) => b.score - a.score);
 
   return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th scope="col">POSITION</th>
-            <th scope="col">PLAYER</th>
-            <th scope="col">SCORE</th>
-          </tr>
-        </thead>
-        <tbody>
-          {rank.map(({ name, score }, index) => (
-            <tr key={index + 1}>
-              <th scope="row">{index + 1}</th>
-              <td>{name}</td>
-              <td>{score}</td>
+    <article className="rankPaneldiv">
+      <section className="tableRank">
+        <table>
+          <thead>
+            <tr>
+              <th scope="col">POSITION</th>
+              <th scope="col">PLAYER</th>
+              <th scope="col">SCORE</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {rank.map(({ name, score }, index) => (
+              <tr key={index + 1}>
+                <th scope="row">{index + 1}</th>
+                <td>{name}</td>
+                <td>{score}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </section>
+    </article>
   );
 }
 
--- src/pages/Rank/Rank.jsx
+++ src/pages/Rank/Rank.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import RankPanel from "../../components/RankPanel/RankPanel";
 import { getRank } from "../../utils/CardCollectionWS";
+import "./Rank.css";
 
 function Rank() {
   const theRank = getRank("rank");
 
   return (
-    <div>
+    <div className="rank">
       <RankPanel theRank={theRank} />
     </div>
   );
